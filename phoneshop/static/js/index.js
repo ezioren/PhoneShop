138f06bf54bfd8cb24aa25a7d295a7403a69f046
reyn <reynren@vectorbuilder.net>
2019-01-17 09:08:31 +0800

    静态文件

---
+++ phoneshop/static/js/index.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    //顶部购物车
+    $('nav .nav-cart').mouseenter(function () {
+        $(this).find('.top-cart').addClass('top-cart-active');
+        $('#top-cart-menu').animate({
+            height:'toggle'},'slow');
+    })
+    $('nav .nav-cart').mouseleave(function (){
+                $('#top-cart-menu').animate({
+                        height:'toggle'
+                    },'slow', function () {
+                });
+                $(this).find('.top-cart').removeClass('top-cart-active');
+
+    });
+
+})
