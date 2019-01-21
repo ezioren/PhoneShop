57e212136342e1330feb25029d442b0289c3fb0e
reyn <reynren@vectorbuilder.net>
2019-01-21 17:48:12 +0800

    更新一些代码

--- phoneshop/static/js/index.js
+++ phoneshop/static/js/index.js
@@ -12,5 +12,24 @@ $(function () {
                 $(this).find('.top-cart').removeClass('top-cart-active');
 
     });
+    //轮播图
+    var swiper = new Swiper('#goods-show .container', {
+        loop: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 100000,
+            disableOnInteraction: false,
+        },
+        autoplayDisableOnInteraction: false,
+        pagination: {
+            el: '#goods-show .goods-show-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '#goods-show .button-next',
+            prevEl: '#goods-show .button-prev',
+        },
 
+    });
 })
