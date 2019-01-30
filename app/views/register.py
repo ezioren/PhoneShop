e03f480cbfc035849f233fc41d912e8de3f186a8
reyn <reynren@vectorbuilder.net>
2019-01-30 13:48:09 +0800

    改良写法，压缩资源

--- app/views/register.py
+++ app/views/register.py
@@ -5,6 +5,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+from app.models.user import *
+
 class RegisterView(APIView):
     def get(self, request):
         return render(request, 'ps_user/register.html', context={'title':'注册'})
